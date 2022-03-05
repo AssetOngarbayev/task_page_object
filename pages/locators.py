83175fc6832a5f08af8f627446f1452543753525
Asset Ongarbayev <asset.ongarbayev@dataart.com>
2022-03-05 16:23:43 +0600

    Переходы между страницами

---
+++ pages/locators.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.ID, "registration_link")
+class LoginPageLocators():
+    LOGIN_PAGE = (By.ID, "login_link")
+    
